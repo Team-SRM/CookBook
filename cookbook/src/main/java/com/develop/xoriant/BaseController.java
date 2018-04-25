8db3f5894fdfb45d1d79cfd19526da8e627974a7
Pritesh Baviskar <pritesh.baviskar@sap.com>
2018-04-25 16:41:31 +0530

    Created a basic mvn webapp archetype with base controller class.

---
+++ cookbook/src/main/java/com/develop/xoriant/BaseController.java
@@ -0,0 +1,19 @@
+package com.develop.xoriant;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Pritesh Baviskar
+ * Date: 4/25/2018
+ */
+
+@Controller
+public class BaseController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String welcome() {
+        return "Hello lalala!";
+    }
+}
